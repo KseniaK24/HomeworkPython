470880329d6ccf96311a7f69e9fd1c80a4d037c8
ksenia <knyazeva_2011@bk.ru>
2022-12-12 17:07:58 +0300

    Homework9

---
+++ Homework9.py
@@ -0,0 +1,11 @@
+# from progress.bar import Bar
+
+# bar = Bar('Processing', max=30)
+# for i in range(30):
+#     # Do some work
+#     bar.next()
+# bar.finish()
+
+# from isOdd import isOdd
+
+# print(isOdd(4)) 
